6e0bb7f119ad291bf08a887b3eab1e0c7b7bed4d
Gavi Galloway <gsgalloway@users.noreply.github.com>
2023-10-17 15:36:49 -0700

    chore: update OpenAPI spec and tests to use https for nemes.farcaster.xyz
    affects: @standard-crypto/farcaster-js-hub-rest

--- packages/farcaster-js-hub-rest/src/openapi/generated/base.ts
+++ packages/farcaster-js-hub-rest/src/openapi/generated/base.ts
@@ -19,7 +19,7 @@ import type { Configuration } from './configuration';
 import type { AxiosPromise, AxiosInstance, AxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = "http://hub.farcaster.standardcrypto.vc:2281".replace(/\/+$/, "");
+export const BASE_PATH = "https://hub.farcaster.standardcrypto.vc:2281".replace(/\/+$/, "");
 
 /**
  *
