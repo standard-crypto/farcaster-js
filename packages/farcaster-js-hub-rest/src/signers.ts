e16f4132f977cbdd1f2085b87504f5869751b176
payton <12602866+payton@users.noreply.github.com>
2024-05-01 13:06:27 -0400

    Update documentation and error message for ExternalEd25519Signer

--- packages/farcaster-js-hub-rest/src/signers.ts
+++ packages/farcaster-js-hub-rest/src/signers.ts
@@ -13,8 +13,8 @@ export class ExternalEd25519Signer extends Ed25519Signer {
      * Creates a new ExternalEd25519Signer. This class allows the signing of Farcaster message hashes to be delegated to an external service
      * such that a private key is not required to be in memory.
      * 
-     * @param signMessageHash Proxy function that signs a message hash. Called internally by signMessageHash.
-     * @param getSignerKey Proxy function that returns the public key of the signer. Called internally by getSignerKey.
+     * @param signMessageHash Proxy function that signs a message hash. Called internally by _signMessageHash.
+     * @param getSignerKey Proxy function that returns the public key of the signer. Called internally by _getSignerKey.
      */
     constructor(signMessageHash: (messageHash: Uint8Array) => Promise<Uint8Array>, getSignerKey: () => Promise<Uint8Array>) {
       super();
@@ -46,7 +46,7 @@ export class ExternalEd25519Signer extends Ed25519Signer {
             const signature = await this._signMessageHash(hash);
             return ok(signature);
         } catch (e) {
-            return err(new HubError("unknown", e instanceof Error ? e.message : "Unable to get signer key"));
+            return err(new HubError("unknown", e instanceof Error ? e.message : "Unable to sign message hash"));
         }
     }
     
