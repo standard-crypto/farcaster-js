2714c36f6267bc00f30ac14df0adbf1551f14cea
payton <12602866+payton@users.noreply.github.com>
2024-05-03 09:56:47 -0700

    Add example using an external signer to wrap the noble signer

--- examples/hubRest.ts
+++ examples/hubRest.ts
@@ -1,11 +1,27 @@
-import { HubRestAPIClient } from '@standard-crypto/farcaster-js';
+import { HubRestAPIClient, ExternalEd25519Signer } from '@standard-crypto/farcaster-js';
 
 const client = new HubRestAPIClient();
 console.log(await client.getHubInfo());
 
+// Use a private key
 const signerPrivateKey = '0x...';
 const fid = 111;
 const writeClient = new HubRestAPIClient({ hubUrl: 'https://hub.farcaster.standardcrypto.vc:2281' });
 
 const publishCastResponse = await writeClient.submitCast({ text: 'This is a test cast submitted from farcaster-js' }, fid, signerPrivateKey);
 console.log(`new cast hash: ${publishCastResponse.hash}`);
+
+// Use an external signer
+import { NobleEd25519Signer } from '@farcaster/core';
+
+const nobleSigner = new NobleEd25519Signer(new Uint8Array([...]));
+const _signMessage = async (messageHash: Uint8Array) => {
+    return nobleSigner.signMessageHash(messageHash);
+}
+const _getPublicKey = async () => {
+    return nobleSigner.getSignerKey();
+}
+const externalSigner = new ExternalEd25519Signer(_signMessage, _getPublicKey);
+
+const publishCastExternalSignerResponse = await writeClient.submitCast({ text: 'This is a test cast submitted from farcaster-js using an external signer' }, fid, externalSigner);
+console.log(`new cast hash with external signer: ${publishCastExternalSignerResponse.hash}`);
